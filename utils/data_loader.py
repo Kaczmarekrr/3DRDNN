f51a0018300bbd19b12db78781748a66b23e8996
Rafal Kaczmarek <r.kaczmarek2@student.uw.edu.pl>
2023-02-23 19:35:24 +0100

    data generator processing

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,24 +1,26 @@
 import os
-import sys
-from glob import glob
-from pathlib import Path
-from tqdm import tqdm
-import numpy as np
-import nibabel as nib
 import SimpleITK as sitk
 import numpy as np
-from matplotlib import pyplot as plt
-import tensorflow as tf
 
 class NiiDataLoader():
     def __init__(self):
         self.path = "data/LITS_Challenge/Training_Batch 1/"
-    def basic_data_generator(self,file_list:list)->np.ndarray:
-        for file in file_list:
-            # Read the .nii image containing the volume with SimpleITK:
-            file_path = self.path+file
-            print(file_path)
-            sitk_t1 = sitk.ReadImage(file_path)
+
+    def preprossing_2d(self,img)->np.ndarray:
+        print("processing...")
+        return img
+
+    def reading_data(self,path:str)->np.ndarray:
+            sitk_t1 = sitk.ReadImage(path)
             t1 = sitk.GetArrayFromImage(sitk_t1)
-            for slide_2d in t1:
-                yield slide_2d
+            t1 = self.preprossing_2d(t1)
+
+            return t1
+
+    def data_generator_2d(self,file_list:list):
+        for file in file_list:
+            file_path = self.path+file
+            img_3d = self.reading_data(file_path)
+            # Read the .nii image containing the volume with SimpleITK:
+            for slice in img_3d:
+                yield slice
