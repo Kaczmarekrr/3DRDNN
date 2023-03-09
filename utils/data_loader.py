17198a6eaa2d47209325c156193961c0842ab1fc
Rafal Kaczmarek <r.kaczmarek2@student.uw.edu.pl>
2023-03-09 18:37:35 +0100

    working data reading and training - train for 50 epochs for liver segmentation with logging

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -6,13 +6,16 @@ import tensorflow as tf
 import glob
 
 class NiiDataLoader():
-    def __init__(self):
-        self.files_volume = glob.glob('data\\LITS_Challenge\\Training_Batch 1\\volume*.nii')
-        self.files_segmenation =  glob.glob('data\\LITS_Challenge\\Training_Batch 1\\segmentation*.nii')
+    def __init__(self,path):
+        self.files_volume = glob.glob(path + '\\volume*.nii')
+        self.files_segmenation =  glob.glob(path + '\\segmentation*.nii')
+
+        print(f"initalised with path {path}")
+        print(f"files: {len(self.files_volume)},{len(self.files_segmenation)}")
 
     def preprossing_3d(self,t1)->np.ndarray:
-        min = -100
-        max = 300
+        min = 10
+        max = 200
         t1= np.where(t1<min,min,t1)
         t1= np.where(t1>max,max,t1)
         t1 -= min
@@ -31,21 +34,38 @@ class NiiDataLoader():
 
             return t1
 
-    def data_generator_2d(self,valid:int):
-        if valid == 0:
-            files_volume = self.files_volume[0:22]
-            files_segmenation = self.files_segmenation[0:22]
-        elif valid == 1:
-            files_volume = self.files_volume[22:]
-            files_segmenation = self.files_segmenation[22:]
+    def generator_data_len(self):
+         return len(self.files_volume)
+    
+    def data_generator_2d(self):
 
-        for i,file_volume in enumerate(files_volume):
+        for i,file_volume in enumerate(self.files_volume):
             img_3d_volume = self.reading_data(file_volume)
             img_3d_volume = self.preprossing_3d(img_3d_volume)
 
-            img_3d_segmentation = self.reading_data(files_segmenation[i])
+            img_3d_segmentation = self.reading_data(self.files_segmenation[i])
             img_3d_segmentation = self.label_seperator(img_3d_segmentation)
             img_3d_segmentation = tf.keras.utils.to_categorical(img_3d_segmentation,2)
             for j in range(len(img_3d_segmentation)):
-                if np.sum(img_3d_segmentation[j,:,:,1]) > 10:
                     yield (tf.image.resize(img_3d_volume[j,:,:,0:1],[256,256]),tf.image.resize(img_3d_segmentation[j,:,:,0:2],[256,256],method="nearest"))
+
+
+class NPYLoader():
+    def __init__(self,path):
+        self.files_volume = glob.glob(path + '\\*_vol.npy')
+        self.files_segmenation =  glob.glob(path + '\\*_seg.npy')
+
+        print(f"initalised with path {path}")
+        print(f"files: {len(self.files_volume)},{len(self.files_segmenation)}")
+
+    def data_generator_2d(self):
+
+        for i,file_volume in enumerate(self.files_volume):
+            img_2d_vol = np.load(file_volume)
+            img_2d_seg = np.load(self.files_segmenation[i])
+
+            yield (img_2d_vol,img_2d_seg)
+
+         
+
+        
